4e70f094a57213488c4e6ed38986f984de22f3d7
Andrey Ryabtsev <ryab@yandex.ru>
2020-12-22 20:43:00 +0300

    validation v.1

--- scripts/script.js
+++ scripts/script.js
@@ -17,6 +17,82 @@ const cardTemplate = document.querySelector('#cards__item-template');
 const nameText = document.querySelector('.popup__form-input_title');
 const linkText = document.querySelector('.popup__form-input_link');
 
+
+
+//код по вебинару
+
+const formUser = document.querySelector('.popup__form_user');
+
+
+//показывает ошибку
+function showError(form, input) {
+  const error = form.querySelector(`#${input.id}-error`);
+  error.textContent = input.validationMessage;
+  input.classList.add('popup__form-input_type_error');
+};
+
+//скрывает ошибку
+function hideError(form, input) {
+  const error = form.querySelector(`#${input.id}-error`);
+  error.textContent = '';
+  input.classList.remove('popup__form-input_type_error');
+};
+
+//проверяет инпуты на валидность
+function checkInputValidity(form, input) {
+  if (input.validity.valid) {
+    hideError(form, input);
+  } else {
+    showError(form, input);
+  }
+};
+
+function setButtonState(button, isActive) {
+  if (isActive) {
+    button.classList.remove('popup__form-button_disabled');
+    button.disabled = false;
+  } else {
+    button.classList.add('popup__form-button_disabled');
+    button.disabled = true;
+  }
+};
+
+//перебираем нодлист из инпутов и расставляем лиснеры, которые включают текст ошибки и подсвечивают поля
+
+function setEventListener(form) {
+  const inputList = form.querySelectorAll('.popup__form-input');
+  const submitButton = form.querySelector('.popup__form-button');
+  inputList.forEach((input) => {
+    input.addEventListener('input', () => {
+      checkInputValidity(form, input);
+      setButtonState(submitButton, form.checkValidity());
+    });
+  });
+}
+
+function enableValidation() {
+  const forms = document.querySelectorAll('.popup__form');
+  forms.forEach((form) => {
+    setEventListener(form);
+    form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+  });
+};
+
+enableValidation();
+
+//конец кода по вебинару
+
+
+
+
+
+
+
+
+
+
 //open popup by adding class popup_opened
 function openPopup(element) {
   element.classList.add('popup_opened');
@@ -98,6 +174,15 @@ function handleFormUser(evt) {
 function openPopupAddCard() {
   openPopup(popupAddCard);
 }
+
+
+/* function setClickOnOverlay(popup) {
+  popup.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('popup')) {
+      closePopup(popup);
+    }
+  });
+} */
 
 userEditButton.addEventListener('click', openPopupUser);
 popupCloseButton.addEventListener('click', closePopupUser);
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,8 +1,75 @@
-enableValidation({
+const formUser = document.querySelector('.popup__form_user');
+
+const config = {
   formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-});
+  inputSelector: '.popup__form-input',
+  submitButtonSelector: '.popup__form-button',
+  inactiveButtonClass: 'popup__form-button_disabled',
+  inputErrorClass: 'popup__form-input_type_error',
+};
+
+//показывает ошибку
+function showError(form, input, config) {
+  const error = form.querySelector(`#${input.id}-error`);
+  error.textContent = input.validationMessage;
+  input.classList.add(config.inputErrorClass);
+};
+
+//скрывает ошибку
+function hideError(form, input, config) {
+  const error = form.querySelector(`#${input.id}-error`);
+  error.textContent = '';
+  input.classList.remove(config.inputErrorClass);
+};
+
+//проверяет инпуты на валидность
+function checkInputValidity(form, input) {
+  if (input.validity.valid) {
+    hideError(form, input);
+  } else {
+    showError(form, input);
+  }
+};
+
+function setButtonState(button, isActive, config) {
+  if (isActive) {
+    button.classList.remove(config.inactiveButtonClass);
+    button.disabled = false;
+  } else {
+    button.classList.add(config.inactiveButtonClass);
+    button.disabled = true;
+  }
+};
+
+//перебираем нодлист из инпутов и расставляем лиснеры, которые включают текст ошибки и подсвечивают поля
+
+
+function setEventListener(form, config) {
+  const inputList = form.querySelectorAll(config.inputSelector);
+  const submitButton = form.querySelector(config.submitButtonSelector);
+  inputList.forEach((input) => {
+    input.addEventListener('input', () => {
+      checkInputValidity(form, input);
+      setButtonState(submitButton, form.checkValidity());
+    });
+  });
+}
+
+setEventListener(formUser, config)
+
+/* function enableValidation(config) {
+  const forms = document.querySelectorAll(config.formSelector);
+  forms.forEach((form) => {
+    setEventListener(form, config);
+    form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    //!!!! вот тут не работает:
+    const submitButton = form.querySelector(config.submitButtonSelector);
+    console.log(submitButton);
+
+    setButtonState(submitButton, form.checkValidity());
+  });
+};
+
+enableValidation(); */
