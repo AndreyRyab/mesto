e3de9327d5550a2f3553ee8025925c86d9c7bfe0
Andrey Ryabtsev <ryab@yandex.ru>
2021-01-11 21:03:17 +0300

    card.js_working

--- scripts/Card.js
+++ scripts/Card.js
@@ -31,26 +31,22 @@ export class Card {
   }
 
   _setEventListeners() {
-    this._element.querySelector('.cards__like-button').addEventListener('click', function (evt) {
-      evt.target.classList.toggle('cards__like-button_active');
-    })/* ('click', this._handleLikeButton) */;
-    this._element.querySelector('.cards__trash-button').addEventListener('click', function (evt) {
-      const targetItem = evt.target.closest('.cards__item');
-      targetItem.remove();
-    });/* ('click', _handleTrashButton); */
-    /* this._element.querySelector('.cards__img').addEventListener('click', _handleFullImage); */
-    this._element.querySelector('.cards__img').addEventListener('click', openFullImage);
+    this._element.querySelector('.cards__like-button').addEventListener('click', this._handleLikeButton);
+
+    this._element.querySelector('.cards__trash-button').addEventListener('click', this._handleTrashButton);
+
+    this._element.querySelector('.cards__img').addEventListener('click', this._handleFullImage);
   }
 
- /*  _handleLikeButton() {
-    this._element.classList.toggle('cards__like-button_active');
-  } */
+  _handleLikeButton() {
+    this.classList.toggle('cards__like-button_active');
+  }
 
- /*  _handleTrashButton() {
-    this._element.closest('.cards__item').remove();
-  } */
+  _handleTrashButton() {
+    this.closest('.cards__item').remove();
+  }
 
-  _handleFullImage() {
+  _handleFullImage(evt) {
     openFullImage(evt);
   }
 
