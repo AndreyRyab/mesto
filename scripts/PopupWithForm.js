fd963e8d8ce9f5a1968db4c160374c7658b91cc9
Andrey Ryabtsev <ryab@yandex.ru>
2021-02-02 15:47:29 +0300

    ready for webpack

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -7,25 +7,17 @@ export default class PopupWithForm extends Popup {
     this._submitForm = submitForm;
   }
 
- /*  _getInputValues() {
-    if (this._form.title) {
-      this._title = this._form.input.title.value;
-      this._link = this._form.link.value;
-    } */
-          
-      /* было:
-      name: this._form.username.value,
-      job: this._form.userjob.value,
-      title: this._form.title.value,
-      link: this._form.link.value */
+  _getInputValues() {
+    this.inputData = { name: this.form.title.value, link: this.form.link.value }
+    }
    
-  
   setEventListeners() {
     super.setEventListeners();
     this.form.addEventListener('submit', this._submitForm);
   }
 
   close() {
+    this._getInputValues();
     super.close();
     this.form.reset();
   }
