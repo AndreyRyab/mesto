fd441aab1b2b72308189fb7af82c7d23c7d76d27
Andrey Ryabtsev <ryab@yandex.ru>
2021-02-11 18:11:07 +0300

    profile editing

--- src/index.js
+++ src/index.js
@@ -92,7 +92,9 @@ const popupProfile = new PopupWithForm('.popup_edit-user-profile', (evt) => {
   //collecting data from the form >>>>>
   userInfo.name = popupProfile.form.username.value;
   userInfo.job = popupProfile.form.userjob.value;
-  userInfo.setUserInfo(); //<<<< setting to the profile
+  userInfo.setUserInfo();
+  api.sendUserInfoToServer();
+  //<<<< setting to the profile
   popupProfile.close();
 });
 //<<<<< profile popup initialisation
@@ -116,40 +118,3 @@ userEditButton.addEventListener('click', () => {
 const popupFullImage = new Popup('.popup_full-image');
 popupFullImage.setEventListeners();
 //<<<<< full image popup initialisation
-
-
-
-
-
-
-
-
-
-
-
-/* fetch('https://mesto.nomoreparties.co/v1/cohort-20/cards', {
-  headers: {
-    authorization: 'fb75d0e9-391a-4d96-80ba-b4913a49b17c'
-  }
-})
-  .then(res => res.json())
-  .then((result) => {
-    console.log(result);
-  }); */
-
-
-
-  //Загрузка информации о пользователе с сервера>>>
-
-/*  fetch('https://mesto.nomoreparties.co/v1/cohort-20/users/me', {
-  method: 'GET',
-  headers: {
-    authorization: 'fb75d0e9-391a-4d96-80ba-b4913a49b17c'
-  }
-})
-  .then(res => res.json())
-  .then((result) => {
-    console.log(result);
-  }); */
-
-  //<<< загрузка информации о пользователе с сервера
